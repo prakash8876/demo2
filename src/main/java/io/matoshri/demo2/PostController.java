e3571b1b0014a18b7ba7b2a5bc9eff771300c234
Prakash <prakash8876@gmail.com>
2026-02-08 03:32:35 +0530

    feat: added fetch by id api

--- src/main/java/io/matoshri/demo2/PostController.java
+++ src/main/java/io/matoshri/demo2/PostController.java
@@ -2,6 +2,7 @@ package io.matoshri.demo2;
 
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +11,19 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/{version}/posts", version = "1")
 public class PostController {
-    private  final PostService postService;
-    public PostController(PostService postService, PostService postService1) {
-        this.postService = postService1;
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
     }
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Post> getPosts() {
         return postService.getPosts();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Post getPostById(@PathVariable("id") Integer id) {
+        return postService.getPostById(id);
     }
 }
--- src/main/java/io/matoshri/demo2/PostService.java
+++ src/main/java/io/matoshri/demo2/PostService.java
@@ -1,5 +1,7 @@
 package io.matoshri.demo2;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
 
@@ -9,4 +11,7 @@ import java.util.List;
 public interface PostService {
     @GetExchange
     List<Post> getPosts();
+
+    @GetExchange("/{id}")
+    Post getPostById(@PathVariable("id") Integer id);
 }
